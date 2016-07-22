e088485cd0e98dc5fdada489aaabfd13359685b7
Aditi Raveesh <aditi.raveesh@gmail.com>
2016-07-22 16:06:39 +0530

    For loops

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,16 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	length := 0
+
+	for n != 1 {
+		if n % 2 == 0 {
+			n /= 2
+		} else {
+			n = 3 * n + 1
+		}
+		length ++
+	}
+
+	return length
 }
